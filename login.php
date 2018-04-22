0a89add2fd9f6bc8b1a4e53dbad2b98ba97acc3b
Divyanshu Tyagi <divyanshutyagi8@gmail.com>
2018-04-22 22:54:36 +0530

    Bootstrap Updated

---
+++ login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE .html>
+<html lang = "en">
+    <head>
+        <meta charset = "utf-8">
+        <title>Login| Medicine App</title>
+        <link rel = "stylesheet" href = "css/bootstrap.min.css">
+    </head>
+<body>  
+    <nav class = "navbar .navbar-default">
+        
+    </nav>
+</body>
+</html>
